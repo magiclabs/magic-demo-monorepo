9de9d46c652c0412d982342cab007fab8c44d33c
Josh Scanlan <joshuascan@gmail.com>
2025-08-29 15:53:13 -0700

    use env vars

--- app/lib/tee-client.ts
+++ app/lib/tee-client.ts
@@ -3,16 +3,13 @@ import { TeeEndpoint } from "./tee-types";
 const TEE_BASE = "https://tee.express.magiclabs.com";
 
 export async function tee(path: TeeEndpoint, jwt: string, init?: RequestInit) {
-  const apiKey = "pk_live_BAF12F1CC6EBF5BE";
-  const oidcProviderId = "3a9StGIjw0aLTsF4RKFgAeX9hkHJwa3LOXKnlmCLqFQ=";
-
   return fetch(TEE_BASE + path, {
     ...init,
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${jwt}`,
-      "X-Magic-API-Key": apiKey,
-      "X-OIDC-Provider-ID": oidcProviderId,
+      "X-Magic-API-Key": process.env.MAGIC_API_KEY ?? "",
+      "X-OIDC-Provider-ID": process.env.OIDC_PROVIDER_ID ?? "",
       "X-Magic-Referrer":
         "https://nextauth-api-wallets-express-demo.vercel.app",
       ...(init?.headers || {}),
