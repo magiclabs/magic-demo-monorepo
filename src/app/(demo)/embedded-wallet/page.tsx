12acfb2808fe306fc63de7e72d20577e65eb3b3d
sherzod-bakhodirov <sherzod.bakhodirov@magic-ext.com>
2025-10-10 15:46:34 +0500

    chore: disable social logins

--- src/app/(demo)/embedded-wallet/page.tsx
+++ src/app/(demo)/embedded-wallet/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
           <EmailOTPAuth />
 
           {/* OAuth Authentication */}
-          <OAuthAuth />
+          {/* <OAuthAuth /> */}
         </div>
       </div>
     </div>
