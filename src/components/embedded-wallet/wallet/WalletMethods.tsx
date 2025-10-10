8dc313388d32a514459474473f20417c33a5b4d4
sherzod-bakhodirov <sherzod.bakhodirov@magic-ext.com>
2025-10-10 19:26:23 +0500

    feat: add user & wallet methods

--- src/components/embedded-wallet/wallet/WalletMethods.tsx
+++ src/components/embedded-wallet/wallet/WalletMethods.tsx
@@ -3,11 +3,53 @@ import { MagicService } from "@/lib/embedded-wallet/get-magic";
 
 const tabs = [
   {
+    value: "connect-with-ui",
+    label: "Connect With UI",
+    functionName: "magic.wallet.connectWithUI()",
+    payload: null,
+    handler: () => MagicService.magic.wallet.connectWithUI(),
+  },
+  {
+    value: "show-ui",
+    label: "Show UI",
+    functionName: "magic.wallet.showUI()",
+    payload: null,
+    handler: () => MagicService.magic.wallet.showUI(),
+  },
+  {
+    value: "show-address",
+    label: "Show Address",
+    functionName: "magic.wallet.showAddress()",
+    payload: null,
+    handler: () => MagicService.magic.wallet.showAddress(),
+  },
+  {
+    value: "show-send-tokens-ui",
+    label: "Show Send Tokens UI",
+    functionName: "magic.wallet.showSendTokensUI()",
+    payload: null,
+    handler: () => MagicService.magic.wallet.showSendTokensUI(),
+  },
+  {
+    value: "show-nfts",
+    label: "Show NFTs",
+    functionName: "magic.wallet.showNFTs()",
+    payload: null,
+    handler: () => MagicService.magic.wallet.showNFTs(),
+  },
+  {
     value: "show-on-ramp",
     label: "Show On Ramp",
     functionName: "magic.wallet.showOnRamp()",
     payload: null,
     handler: () => MagicService.magic.wallet.showOnRamp(),
+  },
+  {
+    value: "show-balances",
+    label: "Show Balances",
+    functionName: "magic.wallet.showBalances()",
+    payload: null,
+    handler: () => MagicService.magic.wallet.showBalances(),
   },
 ];
 
