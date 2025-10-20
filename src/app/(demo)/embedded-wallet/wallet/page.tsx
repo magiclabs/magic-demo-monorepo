d5e7f404fcaddc334748ca88462855f7a77197c6
Josh Scanlan <joshuascan@gmail.com>
2025-10-20 10:22:19 -0700

    hides wallet module

--- src/app/(demo)/embedded-wallet/wallet/page.tsx
+++ src/app/(demo)/embedded-wallet/wallet/page.tsx
@@ -35,7 +35,7 @@ export default function WalletPage() {
             )}
 
             <UserMethods />
-            <WalletMethods />
+            {/* <WalletMethods /> */}
           </div>
         </div>
       </div>
